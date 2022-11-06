806f1d591e953dfbd6d7bc86931e96f977fe747d
Tarık Yeşilçimen <tarikyesilcimenn@gmail.com>
2022-11-06 17:25:41 +0300

    chat-app
    navbar added

---
+++ src/components/Navbar.vue
@@ -0,0 +1,18 @@
+<template>
+  <nav>
+    <div>
+        <p>Hello user</p>
+        <p class="email">User login with </p>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- src/views/ChatRoom.vue
+++ src/views/ChatRoom.vue
@@ -1,10 +1,16 @@
 <template>
-  <h3>Chat room</h3>
+  <div class="container">
+    <Navbar/>
+  </div>
+
 </template>
 
 <script>
+import Navbar from '../components/Navbar.vue'
 export default {
-
+  components:{
+    Navbar
+  }
 }
 </script>
 

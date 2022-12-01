71e0bcf89b3eaa07c8b147427ebee4546c16da5a
tarikyesilcimenn <tarik.yesilcimen@inverizo.com>
2022-12-01 10:08:52 +0300

    navbar
    navbar added

---
+++ src/components/NavBar.vue
@@ -0,0 +1,21 @@
+<template>
+    <nav>
+        <div>
+            <p>Merhaba kullanıcı_ad</p>
+            <p class="email">email_adres ile giriş yaptınız</p>
+        </div>
+        <button @click="logOut">Çıkış Yap</button>
+    </nav>
+</template>
+
+<script>
+import { Auth } from '@firebase/auth';
+import { useRouter } from 'vue-router';
+
+export default {
+    setup() {
+        const router = useRouter();
+        
+    },
+}
+</script>
--- src/views/ChatRoom.vue
+++ src/views/ChatRoom.vue
@@ -1,10 +1,13 @@
 <template>
-  <h3>Chat room</h3>
+  <div class="container">
+    <Navbar/>
+  </div>
 </template>
 
 <script>
+import Navbar from '../components/NavBar';
 export default {
-
+  components:{Navbar}
 }
 </script>
 
